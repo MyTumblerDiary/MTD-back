1e4ffd604ca4a22d64704997dff0a0cf8df3046d
hong1995 <whwoghd1@naver.com>
2023-03-29 01:15:44 +0900

    refactor: 소셜로그인시 access token발급

--- src/apis/auth/auth.service.ts
+++ src/apis/auth/auth.service.ts
@@ -24,25 +24,16 @@ export class AuthService {
     );
   }
   async loginUser({ email, password, context }) {
-    //1. 로그인(이메일이 일치하는 유저를 DB에서 찾기)
     const user = await this.userService.findOne({ email });
-    //2. 일치하는 유저가 없으면?! 에러 던지기!!!
     if (!user) throw new UnprocessableEntityException('이메일이 없습니다.');
-    //3. 일치하는 유저가 있지만, 비밀번호가 틀렸다면?! 에러던지기
     const isAuth = await bcrypt.compare(password, user.password);
     if (!isAuth)
       throw new UnprocessableEntityException('비밀번호가 틀렸습니다.');
-    //4. refreshToken(=JWT)을 만들어서 프론트엔드(쿠키)에 보내주기
     this.setRefreshToken({ user, res: context.res });
-    // //5. 일치하는 유저가 있으면?! accessToken(=JWT)
     return this.getAccessToken({ user });
   }
   async social_login({ req, res }) {
-    //1. 가입확인
-    console.log(req.user.email);
     let user = await this.userService.findOne({ email: req.user.email });
-    //2. 회원가입
-    console.log(req.user.password);
     const hashedPassword = await bcrypt.hash(req.user.password, 10);
     if (!user) {
       user = await this.userService.create({
@@ -54,8 +45,8 @@ export class AuthService {
         },
       });
     }
-    //3. 로그인
     await this.setRefreshToken({ user, res });
+    await this.getAccessToken({ user });
     res.redirect('http://localhost:5500/front/social-login.html');
   }
 }
