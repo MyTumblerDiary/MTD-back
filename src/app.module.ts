b417c63c91814bf7f844d221679f512be8812e9a
kth5954 <kth5954@naver.com>
2023-04-02 11:29:04 +0000

    feat: CI/CD 및 배포, API limit 등

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,9 @@ import { UserModule } from './apis/users/users.module';
 import { configOptions } from './config/config';
 import { ormOption } from './config/typeorm.config';
 import { DynamicGqlModule } from './dynamic-gql.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { GqlThrottlerGuard } from './commons/guards/gql.throttler.guard';
 
 const ENV = process.env.NODE_ENV;
 
@@ -18,12 +21,20 @@ const ENV = process.env.NODE_ENV;
     AuthModule,
     DynamicGqlModule.forRoot(),
     TypeOrmModule.forRoot(ormOption),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 20,
+    })
   ],
   providers: [
     {
       provide: 'NODE_ENV',
       useValue: ENV,
     },
+    {
+      provide: APP_GUARD,
+      useClass: GqlThrottlerGuard
+    }
   ],
 })
-export class AppModule {}
+export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('SERVER_PORT');
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept,Authorization',
+    credentials: true,
+  });
   app.get(GraphQLSchemaHost);
+
 
   await app.listen(port);
 }
