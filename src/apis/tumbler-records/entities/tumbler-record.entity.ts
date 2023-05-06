8fca68d6fd5c91bb2f93ce1629f27b4a180209ee
thisistaehun <taehun@tesser.co.kr>
2023-05-07 01:44:05 +0900

    feat: tumbler records add features

--- src/apis/tumbler-records/entities/tumbler-record.entity.ts
+++ src/apis/tumbler-records/entities/tumbler-record.entity.ts
@@ -1,7 +1,8 @@
 import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
+import { Store } from 'src/apis/stores/entities/store.entity';
 import { User } from 'src/apis/users/entities/user.entity';
 import { CommonEntity } from 'src/commons/entities/common.entity';
-import { Column, Entity, ManyToOne } from 'typeorm';
+import { Column, Entity, JoinColumn, ManyToOne } from 'typeorm';
 
 @Entity({})
 @InputType('TumblerRecordInputType', { isAbstract: true })
@@ -10,6 +11,10 @@ export class TumblerRecord extends CommonEntity {
   @Field(() => Int, { description: '텀블러 할인 금액', nullable: false })
   @Column({ type: 'int' })
   prices: number;
+
+  @Field(() => String, { description: '텀블러 기록 메모', nullable: true })
+  @Column({ type: 'text', nullable: true })
+  memo: string;
 
   @Field(() => String, { description: '텀블러 이미지 파일 키', nullable: true })
   @Column({ type: 'text', nullable: true })
@@ -20,5 +25,12 @@ export class TumblerRecord extends CommonEntity {
     nullable: false,
   })
   @ManyToOne(() => User, (user) => user.tumblerRecords)
+  @JoinColumn({ name: 'user_id', referencedColumnName: 'id' })
   user: User;
+
+  @ManyToOne(() => Store, (store) => store.tumblerRecords, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'store_id', referencedColumnName: 'id' })
+  store: Store;
 }
