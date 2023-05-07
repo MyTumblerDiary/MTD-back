2590283285af856289e223d59df5bb7e91b9c9fe
thisistaehun <taehun@tesser.co.kr>
2023-05-07 18:52:29 +0900

    refactor: 폴더 구조 변경

--- src/apis/spaces/stores/stores.module.ts
+++ src/apis/spaces/stores/stores.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { FranchisesModule } from '../franchises/franchises.module';
+import { FranchisesModule } from '../../franchises/franchises.module';
 import { Store } from './entities/store.entity';
 import { StoresResolver } from './stores.resolver';
 import { StoresService } from './stores.service';
