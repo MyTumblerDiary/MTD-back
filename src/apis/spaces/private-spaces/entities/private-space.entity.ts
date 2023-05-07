af218aaf4dbf7201081cbe945f70785c8463311a
thisistaehun <taehun@tesser.co.kr>
2023-05-07 19:03:38 +0900

    fix: refactor tumbler module with changed structure

--- src/apis/spaces/private-spaces/entities/private-space.entity.ts
+++ src/apis/spaces/private-spaces/entities/private-space.entity.ts
@@ -1,7 +1,8 @@
 import { Field, Float, InputType, ObjectType } from '@nestjs/graphql';
+import { Space } from 'src/apis/spaces/interfaces/space.interface';
+import { TumblerRecord } from 'src/apis/tumbler-records/entities/tumbler-record.entity';
 import { CommonEntity } from 'src/commons/entities/common.entity';
-import { Space } from 'src/commons/interfaces/space.interface';
-import { Column, Entity } from 'typeorm';
+import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity()
 @InputType('PrivateSpaceInputType', { isAbstract: true })
@@ -71,4 +72,14 @@ export class PrivateSpace extends CommonEntity implements Space {
       'AWS S3 버킷에 저장되는 개인 공간의 대표 이미지 파일키 입니다. ',
   })
   imageFileKey?: string;
+
+  @OneToMany(
+    () => TumblerRecord,
+    (tumblerRecords) => tumblerRecords.privateSpace,
+  )
+  @Field(() => [TumblerRecord], {
+    nullable: true,
+    description: '개인 공간의 텀블러 기록들입니다. ',
+  })
+  tumblerRecords?: TumblerRecord[];
 }
--- src/apis/tumbler-records/dto/create.tumbler-record.dto.ts
+++ src/apis/tumbler-records/dto/create.tumbler-record.dto.ts
@@ -7,7 +7,13 @@ export class CreateTumblerRecordInput extends PartialType(
 ) {
   @Field(() => String, {
     description: '텀블러를 사용한 가게입니다. ',
-    nullable: false,
+    nullable: true,
   })
-  storeId: string;
+  storeId?: string;
+
+  @Field(() => String, {
+    description: '텀블러를 사용한 개인 공간입니다. ',
+    nullable: true,
+  })
+  privateSpaceId?: string;
 }
